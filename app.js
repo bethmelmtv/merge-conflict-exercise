db6dffdc26b0de0c11969b4a2c3f2b4abcf9a278
JeffAllison <itsjallison@gmail.com>
2022-04-08 12:50:19 -0700

    made a change to app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // import functions and grab DOM elements
 
+const dude =
+
 // let state
 
 // set event listeners 
   // get user input
-  // use user input to update state 
+  // use user input to update state
   // update DOM to reflect the new state
